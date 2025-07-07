d0170b0f9afe3fc3fefa80223dc39bb4c5e99a06
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-07-07 17:53:51 +0530

    add signup api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,18 +3,44 @@ const User = require("../models/user");
 
 const router = express.Router();
 
-router.post("/signup", async(req, res) => {
-    try{
-        const userData = req.body;
-        const user = new User(userData);
+router.post("/signup", async (req, res) => {
+  const data = req.body;
+  const { firstName, lastName, emailId, password } = data;
 
-        const data = await user.save();
+  try {
+    if (
+      firstName.trim().length === 0 ||
+      lastName.trim().length === 0 ||
+      emailId.trim().length === 0 ||
+      password.trim().length === 0
+    ) {
+      throw new Error("Input field is empty");
+    }
 
-        res.send(data);
+    const ALLOWED_DATA = ["firstName", "lastName", "emailId", "password"];
+
+    const isAllowedData = Object.keys(data).every((k) => {
+      return ALLOWED_DATA.includes(k);
+    });
+
+    if (!isAllowedData) {
+      throw new Error("Trying to add extra field");
     }
-    catch(err){
-        res.send("Error: " + err.message);
+
+    data.emailId = data.emailId.toLowerCase();
+    const user = new User(data);
+    const existingEmail = await User.findOne({ emailId: data.emailId });
+
+    if (existingEmail) {
+      throw new Error("User with this email already exists");
     }
+
+    const userData = await user.save();
+
+    res.json({ msg: "User signup successfully", data: userData });
+  } catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
 });
 
 module.exports = router;
