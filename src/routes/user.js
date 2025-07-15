97ca131102b7e2840c6d1de75de639f18bdf92ac
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-07-15 19:36:00 +0530

    add see accepted user api

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ const ConnectionRequest = require("../models/connectionRequest");
 
 const router = express.Router();
 
-const SAVE_USER_DATA = "firstName lastName photoUrl age gender about skills"
+const SAVE_USER_DATA = "firstName lastName photoUrl age gender about skills";
 
 router.get("/user/requests/received", authMiddleware, async (req, res) => {
   try {
@@ -14,12 +14,35 @@ router.get("/user/requests/received", authMiddleware, async (req, res) => {
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate(
-      "fromUserId",
-      SAVE_USER_DATA
-    );
+    }).populate("fromUserId", SAVE_USER_DATA);
 
     res.json({ msg: "Data fetch succesfully", data: connectionRequest });
+  } catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
+});
+
+router.get("/user/connections", authMiddleware, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", SAVE_USER_DATA)
+      .populate("toUserId", SAVE_USER_DATA);
+
+    const data = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data: data });
   } catch (err) {
     res.status(400).send("Error : " + err.message);
   }
